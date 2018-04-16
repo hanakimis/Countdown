49aecb7770d773466d6087a0b5a0354291379396
Hana Kim <hanakimis@gmail.com>
2018-04-15 21:21:06 -0700

    shit is all f-ed up

--- Countdown/ConcentricCirclesViewController.swift
+++ Countdown/ConcentricCirclesViewController.swift
@@ -26,85 +26,129 @@ class ConcentricCirclesViewController: UIViewController {
     var minutes = Ticker()
     var seconds = Ticker()
     
-    var daysLeft: Int!
-    var hoursLeft: Int!
-    var minutesLeft: Int!
-    var secondsLeft: Int!
+    var daysLeft: Int! = 0
+    var hoursLeft: Int! = 0
+    var minutesLeft: Int! = 0
+    var secondsLeft: Int! = 0
     
     var datePickerContainerOpen = false
+    var thereWasAlreadyDate = false
 
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupUI()
+        // set the style for the datepicker, and the date control
+        datePickerChooserStyle()
         
-        updateDifferenceTime()
-        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(ConcentricCirclesViewController.updateTickers), userInfo: nil, repeats: true)
-    }
-    
-    // setup the date style and other UI elements for the ViewController
-    func setupUI() {
-        // setup datepicker
-        formatter.dateStyle = .medium
-        self.datePicker.setValue(UIColor.white, forKeyPath: "textColor")
-        self.datePicker.minimumDate = today
-        datePickerContainerOpen = false
-
-        checkTargetDateSet()
-    }
-    
-    
-    func checkTargetDateSet() {
-        // 1. Get the date
-        // update if a date has been stored
-        // need to do a check to see if the date is before today
+        // if a date is already stored in user defaults, use it
         if let myDate = defaults.object(forKey: "date") {
-            
-            //if a date has been set already, use this as the target date, unless date has past already
-            if countdownDate < Date() {
-                countdownDate = myDate as! Date
-                // set the datepicker to this target date
-                datePicker.date = countdownDate
+            thereWasAlreadyDate = true
+            print("There is a stored date")
 
-                // set the circles
-                setupCircles(countdownDate: countdownDate)
+            countdownDate = myDate as! Date
+
+            if countdownDate > Date() {
+                print("the stored date is later than today - still time left to the stored date")
                 
+                // set the datepicker to this target date by default
+                datePicker.date = countdownDate
+                
+                // set the style of the button text
+                formatter.locale = Locale(identifier: "en_US")
+                let myStringafd = formatter.string(from: Date())
+                changeDateButton.setTitle(myStringafd, for: UIControlState.normal)
+                
+        
+                // set the circles
+                setupCircles()
+                
+                // start the countdown
+                Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(ConcentricCirclesViewController.updateTickers), userInfo: nil, repeats: true)
             } else {
+                print("the stored date has passed")
+
                 // show the date has already passed, and ask user to input a new date
+                // open the datepicker
+
                 displayDatePassed()
                 setDateStartup()
             }
             
         } else {
-            // this means date hasn't been set
+            print("NO stored date exists")
+            // this means date hasn't been set in User Defaults
             // open the datepicker, and setup to input a date
-            // countdownDate = Calendar.current.date(byAdding: .day, value: 1, to: today)!
+            // Temporary setupL: countdownDate = Calendar.current.date(byAdding: .day, value: 1, to: today)!
+            
+
             setDateStartup()
         }
+        
+
     }
+    
+    
+    @IBAction func tapDateButton(_ sender: Any) {
+        today = Date()
+        let components = (Calendar.current as NSCalendar).components([.second, .minute, .hour, .day], from: today,
+                                                                     to: self.datePicker.date, options: [])
+        
+        daysLeft = components.day
+        hoursLeft = components.hour
+        minutesLeft = components.minute
+        secondsLeft = components.second
+     
+//        print("number of days left \(daysLeft)")
+//        print("number of hours left \(hoursLeft)")
+//        print("number of minutes left \(minutesLeft)")
+//        print("number of seconds left \(secondsLeft)")
+        
+        
+        // clicking this button should either open or close the date-picker
+        // probably should check to make sure the date chosen is logical
+        if (self.datePickerContainerOpen) {
+            closeDatePicker()
+        } else {
+            openDatePicker()
+        }
+
+        
+    }
+    
+    
+    
+    
+    // setup the date style and other UI elements for the ViewController
+    func datePickerChooserStyle() {
+        // setup datepicker style
+        formatter.dateStyle = .medium
+        self.datePicker.setValue(UIColor.white, forKeyPath: "textColor")
+        self.datePicker.minimumDate = today
+        datePickerContainerOpen = false
+    }
+    
     
     func displayDatePassed() {
         print("date has passed")
         // show the stuffs
+        
     }
     
+    
+    // this basically pushes the users to add in a date
     func setDateStartup() {
-        //
-        
         // need to intialize the timeleft
         // pieFillView.initialDaysLeft(initialDaysLeft: Int())
         // use nsdefaults to store initialDaysLeft
         
-//        let formatter = DateFormatter()
-//        formatter.locale = Locale(identifier: "en_US")
-//        let myStringafd = formatter.string(from: Date())
-//        changeDateButton.text = formatter.string(from: Date())
+        openDatePicker()
+
     }
     
     
     
-    func setupCircles(countdownDate: Date) {
+    func setupCircles() {
         
         // 0. Set size and location of frames
         circlesContainerView.frame.size.width = self.view.frame.width * 0.8
@@ -143,7 +187,7 @@ class ConcentricCirclesViewController: UIViewController {
         
         // 5. set the dates
         // Figure out the difference between the target date and right now
-        updateDifferenceTime()
+        updateTickers()
         
         hours.initializeStatus(howMany: hoursLeft)
         minutes.initializeStatus(howMany: minutesLeft)
@@ -152,19 +196,26 @@ class ConcentricCirclesViewController: UIViewController {
     }
 
     
-    
-   
-    
-    
     func updateDifferenceTime() {
         today = Date()
         let components = (Calendar.current as NSCalendar).components([.second, .minute, .hour, .day], from: today,
                                                                      to: countdownDate, options: [])
         
+
         daysLeft = components.day
         hoursLeft = components.hour
         minutesLeft = components.minute
         secondsLeft = components.second
+        
+        
+//        print("today's Date: \(today)")
+//        print("countdown Date: \(countdownDate)")
+//
+//        print("days left \(daysLeft)")
+//        print("hours left \(hoursLeft)")
+//        print("minutes left \(minutesLeft)")
+//        print("seconds left \(secondsLeft)")
+
     }
     
 
@@ -177,7 +228,16 @@ class ConcentricCirclesViewController: UIViewController {
         seconds.updateStatus(howMany: secondsLeft)
     }
     
+    
+    
     func openDatePicker() {
+        
+        // check to see if the date is already been there
+        // need to also check to see if the stored date has passed
+        //self.datePicker.date =  Calendar.current.date(byAdding: .day, value: 1, to: Date())!
+        
+        
+        
         UIView.animate(withDuration: 0.3, animations: { () -> Void in
             
             //            self.toggleDateChanger.contentEdgeInsets = UIEdgeInsetsMake(0.0, 0.0, 0.0, 0.0)
@@ -202,6 +262,13 @@ class ConcentricCirclesViewController: UIViewController {
         
         // update the target date
         // update the time left
+        countdownDate = self.datePicker.date
+        defaults.set(countdownDate, forKey: "date")
+        
+        // do we need to do stuff if there was already a date set?
+        setupCircles()
+        updateTickers()
+        
         
         UIView.animate(withDuration: 0.3, animations: { () -> Void in
             
@@ -220,6 +287,7 @@ class ConcentricCirclesViewController: UIViewController {
             
         }, completion: { (Bool) -> Void in
             self.datePickerContainerOpen = false
+            Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(ConcentricCirclesViewController.updateTickers), userInfo: nil, repeats: true)
         })
     }
     
